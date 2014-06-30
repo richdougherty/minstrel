cd76fc413d280a88bdbf02f08155f9231e65514c
Rich Dougherty <rich@rd.gen.nz>
2014-06-30 12:44:58 +1200

    Flatten quotations as a separate compilation stage

--- src/main/scala/com/richdougherty/minstrel/compile/Compiler.scala
+++ src/main/scala/com/richdougherty/minstrel/compile/Compiler.scala
@@ -3,16 +3,35 @@ package com.richdougherty.minstrel.compile
 import com.richdougherty.minstrel._
 import com.richdougherty.minstrel.assemble._
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.immutable
 
 object Compiler {
+  def flatten(program: Program): Program = {
+    val flatDefs = new ArrayBuffer[Def]
+    def flattenDef(df: Def): Unit = {
+      var anonId = 0
+      var newBody = new ArrayBuffer[AST]
+      var quotDefs = new ArrayBuffer[Def]
+      for (ast <- df.body) ast match {
+        case word: Word => newBody += word
+        case num: Num => newBody += num
+        case Quot(body) =>
+          val quotName = df.name+"-"+anonId
+          quotDefs += Def(quotName, body)
+          newBody += Word(quotName)
+      }
+      flatDefs += df.copy(body = newBody.to[immutable.Seq])
+      quotDefs.foreach(flattenDef)
+    }
+    program.defs.foreach(flattenDef)
+    program.copy(defs = flatDefs.to[immutable.Seq])
+  }
+
   def compile(program: Program): Seq[Directive] = {
     val builder = new AssemblyBuilder
-    val workStack = new mutable.Stack[Def]
-    workStack.pushAll(program.defs.reverse)
     val dictionary = StandardImmediates.dictionary
-
-    while (workStack.nonEmpty) {
-      val df = workStack.pop()
+    for (df <- flatten(program).defs) {
       builder.label(df.name)
       for (ast <- df.body) ast match {
         case Word(name) =>
@@ -24,12 +43,11 @@ object Compiler {
           builder.appendAll(wordDirectives)
         case Num(value) =>
           builder.push(value)
-        case Quot(body) => ???
+        case Quot(body) => sys.error("quot not expected in flattened program")
       }
       builder.ret()
     }
 
     builder.directives
-
   }
 }
